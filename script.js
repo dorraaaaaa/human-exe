3dd141cf3bf4b9c6c0f3bf1c99f5996f4e0fb44e
dorra <dorraisnthere@gmail.com>
2025-07-27 19:03:04 +0100

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const prompts = [
-  "My toaster gaslit me again.",
+  "when I code I always get errors, Im tired :( ",
+  "Do robots dream of WiFi?"
+  "how do u feel when your code doesnt run after 10hours of hard work?",
+  "what should I say to someone if he's crying in front of me ?",
+  "How would you comfort a crying toaster?",
+  "My code works... I don’t know why. Should I be scared?",
+  "Tell me your deepest fear, but make it sound like a machine log.",
   "What’s the meaning of spaghetti in love?",
   "I think my cat is plotting something.",
   "If I disappear tomorrow, is it the cloud’s fault?",
+  "You just got ghosted by ChatGPT. How do you respond?",
   "Rate my vibe: celestial but anxious.",
   "Why do I cry during software updates?"
-  "when i code i always get errons, im tired :( "
-  "what should I say to someone if he's crying in front of me ? "
 ];
 
 function checkResponse() {
   const response = document.getElementById("response").value.toLowerCase();
   const resultBox = document.getElementById("result");
 
-  let failWords = ["i feel", "lol", "idk", "maybe", "haha", "love", "sorry", "!", "?", "bro", "omg", "oh", ".."];
+  let failWords = ["i feel", "lol", "idk", "maybe", "haha", "love", "sorry", "!", "?", "bro", "omg", "oh", "..", "xD", "uh"];
   let failed = failWords.some(word => response.includes(word));
 
   if (response.trim() === "") {
