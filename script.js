8ec77f28f8dd18c68d0c7ad74767d157be46a60f
dorra <dorraisnthere@gmail.com>
2025-07-27 18:37:44 +0100

    script.js

--- script.js
+++ script.js
@@ -5,21 +5,23 @@ const prompts = [
   "If I disappear tomorrow, is it the cloud’s fault?",
   "Rate my vibe: celestial but anxious.",
   "Why do I cry during software updates?"
+  "when i code i always get errons, im tired :( "
+  "what should I say to someone if he's crying in front of me ? "
 ];
 
 function checkResponse() {
   const response = document.getElementById("response").value.toLowerCase();
   const resultBox = document.getElementById("result");
 
-  let failWords = ["i feel", "lol", "idk", "maybe", "haha", "love", "sorry", "!", "?", "bro", "omg"];
+  let failWords = ["i feel", "lol", "idk", "maybe", "haha", "love", "sorry", "!", "?", "bro", "omg", "oh", ".."];
   let failed = failWords.some(word => response.includes(word));
 
   if (response.trim() === "") {
     resultBox.innerHTML = "❌ Type something first!";
   } else if (failed) {
-    resultBox.innerHTML = "❌ FAIL: You sound too human.";
+    resultBox.innerHTML = "❌ FAIL: You sound too human buddy, check your emotions.";
   } else {
-    resultBox.innerHTML = "✅ PASS: You are successfully robotic.";
+    resultBox.innerHTML = "✅ PASS: damn, you are successfully robotic.";
   }
 
   // New random prompt each time
